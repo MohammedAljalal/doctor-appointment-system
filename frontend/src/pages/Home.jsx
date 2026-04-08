0f8d553d6881e73297614a7e3fa3ce896a45cf2d
MohammedAlgalal <mohammedalgalalalgalal@gmail.com>
2026-04-08 04:09:29 +0300

    Add comment to clarify purpose of Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import SpecialityMenu from '../component/SpecialityMenu'
 import TopDoctors from '../component/TopDoctors'
 import Banner from '../component/Banner'
 
+// This is the home page of the application which will display the header, speciality menu, top doctors and banner.
+
 const Home = () => {
   return (
     <div>
