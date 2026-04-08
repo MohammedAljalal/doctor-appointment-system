a6368912a3f9742ce1e50a37a6a3b9f3a709a1e5
MohammedAlgalal <mohammedalgalalalgalal@gmail.com>
2026-04-08 03:45:43 +0300

    Prepare frontend for production deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../component/Header'
 import SpecialityMenu from '../component/SpecialityMenu'
-import TopDoctors from '../component/topDoctors'
+import TopDoctors from '../component/TopDoctors'
 import Banner from '../component/Banner'
 
 const Home = () => {
